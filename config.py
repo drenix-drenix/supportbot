87ce236da218221bce2c3076172448193a8a7b0a
drenix-drenix <109510841+drenix-drenix@users.noreply.github.com>
2023-02-18 00:01:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_TOKEN = '5734386273:AAFtoadLPe5DT6c9ZspPn7mIva8dTDOuCI4' # bot token
+API_TOKEN = '5605323770:AAGfV4Egau8wT4vOTRemRZQJESBQDbwLtt8' # bot token
 admin = 1627807288 # id админа (взять можно тут - t.me/userinfobot)
